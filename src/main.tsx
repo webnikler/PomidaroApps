1a895486ceeeaa07bbff848df255cbb398861a9c
webnikler <webnikler@gmail.com>
2024-11-26 23:31:18 +0300

    fix styles import

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './styles/index.css'
+import './app/styles/index.css'
 import App from './app/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
