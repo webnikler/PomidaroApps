06f81426317fc68e54cf33b31842b02e4b71e3ce
webnikler <webnikler@gmail.com>
2024-12-06 00:09:06 +0300

    fix maxValue calculation

--- src/shared/ui/Chart/components/Provider/useMaxValueStore.ts
+++ src/shared/ui/Chart/components/Provider/useMaxValueStore.ts
@@ -3,7 +3,7 @@ import { useMap } from 'usehooks-ts';
 
 export const useMaxValueStore = () => {
   const [store, { set, remove }] = useMap<string, number>([]);
-  const maxValue = useMemo(() => Math.max(...store.values()), [store]);
+  const maxValue = useMemo(() => Math.max(...store.values(), 0), [store]);
 
   return { setMaxValue: set, removeMaxValue: remove, maxValue };
 };
